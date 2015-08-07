2403c8d79efadfd8162e3f74e554ce834cdd81d7
udaysagar2177 <udaysagar.2177@gmail.com>
2015-08-07 00:15:40 +0000

    Increased puppet quality score to 4.6

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,29 @@
-class send_collectd_metrics(
+# This module that configures write_http plugin of collectd 
+# to send the system metrics to SignalFx
+#
+class send_collectd_metrics (
 
-        $api_token = "", # (required parameter)
-	$dimension_list = {},
-	$set_aws_instanceId = false,
-	$signalfx_url = "https://ingest.signalfx.com/v1/collectd"
+  $api_token                 = '', # (required parameter)
+  $dimension_list            = {},
+  $set_aws_instanceId        = false,
+  $signalfx_url              = 'https://ingest.signalfx.com/v1/collectd'
 ) {
-	include 'collectd'
+  include 'collectd'
         
         $dimensions = get_dimensions($dimension_list, $set_aws_instanceId)
         
-        $url = "${signalfx_url}${dimensions}"
+        $url        = "${signalfx_url}${dimensions}"
 
-	notify {"Collectd will transmit metrics to this url: ${url}":}
+        notify {"Collectd will transmit metrics to this url: ${url}":}
 
         class { 'collectd::plugin::write_http':
-            urls => {
-               "${url}"  => { 'user' => "auth", 'password' => $api_token, 'format' => 'JSON' },
+            urls      => {
+            "${url}"    => {
+                          'user'                       => 'auth',
+                          'password'                   => $api_token,
+                          'format'                     => 'JSON'
             },
-            notify => Service['collectd'],
+            },
+            notify    => Service['collectd'],
         }
 }
