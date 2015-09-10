3274be5a695201db39e745056133b78464effd30
udaysagar2177 <udaysagar.2177@gmail.com>
2015-09-10 01:37:32 +0000

    Updated AWS metadata collection

--- lib/puppet/parser/functions/get_dimensions.rb
+++ lib/puppet/parser/functions/get_dimensions.rb
@@ -1,10 +1,10 @@
 Puppet::Parser::Functions.newfunction(:get_dimensions, :type => :rvalue) do |args|
 dimension_list = args[0]
-set_aws_instanceId = args[1]
+set_aws_instanceRegion = args[1]
 DIMENSIONS = "?"
-if set_aws_instanceId
-        puts "Getting AWS instance ID..."
-	uri = URI.parse("http://169.254.169.254/latest/meta-data/instance-id")
+if set_aws_instanceRegion
+        puts "Getting AWS instanceRegion..."
+        uri = URI.parse("http://169.254.169.254/latest/dynamic/instance-identity/document")
         http = Net::HTTP.new(uri.host, uri.port)
         http.open_timeout = 4
         http.read_timeout = 4
@@ -13,13 +13,14 @@ if set_aws_instanceId
                 begin
                         response = http.request(Net::HTTP::Get.new(uri.request_uri))
                 rescue Timeout::Error
-			puts "ERROR: Unable to get AWS instance ID, Timeout due to reading"
+                       puts "ERROR: Unable to get AWS instance ID, Timeout due to reading"
                 end
         rescue Timeout::Error
                 puts "ERROR: Unable to get AWS instance ID, Timeout due to connecting"
         end
         unless response.nil? || response == 0
-                DIMENSIONS << "sfxdim_InstanceId=#{response.body}&"
+                result = JSON.parse(response.body)
+                DIMENSIONS << "sfxdim_InstanceRegion=#{result["instanceId"]}_#{result["region"]}_#{result["accountId"]}&"
         end
 end
 unless dimension_list.empty?
